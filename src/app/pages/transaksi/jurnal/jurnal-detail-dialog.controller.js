6b6758604849cc9d64d97cc11d3cc89969c939cf
faris <faris@thegreatsoft.com>
2017-05-12 15:22:33 +0700

    trial tree master detail lookupProyek

--- src/app/pages/transaksi/jurnal/jurnal-detail-dialog.controller.js
+++ src/app/pages/transaksi/jurnal/jurnal-detail-dialog.controller.js
@@ -15,7 +15,7 @@
         ctrl.lookupProyek = lookupProyek;
         ctrl.lookupAkun = lookupAkun;
         ctrl.cariAkunByKode = cariAkunByKode;
-        $scope.modalTitle = (entity.akun === null || entity.akun === undefined)?"Tambah Detail Jurnal":"Ubah Detail Jurnal";
+        $scope.modalTitle = (entity.akun === null || entity.akun === undefined) ? "Tambah Detail Jurnal" : "Ubah Detail Jurnal";
         console.log('ctrl.data', ctrl.data);
         $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
@@ -55,11 +55,12 @@
             }
         }
 
-        function lookupProyek() {
+        function lookupProgram() {
+            console.log('Open modal');
             var modalInstance = $uibModal.open({
                 animation: true,
-                templateUrl: 'app/pages/template/lookup-proyek/lookup-proyek.html',
-                controller: 'LookupProyekController',
+                templateUrl: 'app/pages/template/lookupProgramTree/lookup-program-tree.html',
+                controller: 'LookupProgramTreeController',
                 controllerAs: 'ctrl',
                 size: 'lg',
                 resolve: {
@@ -75,8 +76,41 @@
                 }
             });
             modalInstance.result.then(function (selectedItem) {
-                ctrl.data.mataAnggaran = selectedItem;
+                ctrl.data.program = selectedItem;
+                if (ctrl.data.proyek !== null && ctrl.data.program.id !== ctrl.data.program.id) {
+                    ctrl.data.proyek = null;
+                }
+                console.log('ctrl.data.program', ctrl.data.program);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        }
 
+        function lookupProyek() {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'app/pages/template/lookupProyekTree/lookup-proyek-tree.html',
+                controller: 'LookupProyekTreeController',
+                controllerAs: 'ctrl',
+                size: 'lg',
+                resolve: {
+                    lookupPagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                            return {
+                                page: PaginationUtil.parsePage($stateParams.page),
+                                sort: $stateParams.sort,
+                                predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                                ascending: PaginationUtil.parseAscending($stateParams.sort),
+                                search: $stateParams.search
+                            };
+                        }],
+                    program: ctrl.data.program
+                }
+            });
+            modalInstance.result.then(function (selectedItem) {
+                ctrl.data.proyek = selectedItem;
+                if (ctrl.data.program===undefined || ctrl.data.program.id === null || ctrl.data.program.id === undefined) {
+                    ctrl.data.program = ctrl.data.proyek.program;
+                }
             }, function () {
                 $log.info('Modal dismissed at: ' + new Date());
             });
