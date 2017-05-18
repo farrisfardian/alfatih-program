48d0ae3cf658219c241cfaf339ffb709052d6712
faris <faris@thegreatsoft.com>
2017-05-18 17:01:59 +0700

    menambahkan webservice untuk jurnal outstanding (belum terposting)

--- src/main/java/id/alfatih/repository/akuntansi/JurnalRepository.java
+++ src/main/java/id/alfatih/repository/akuntansi/JurnalRepository.java
@@ -1,6 +1,7 @@
 package id.alfatih.repository.akuntansi;
 
 import id.alfatih.domain.akuntansi.Jurnal;
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,8 +19,14 @@ public interface JurnalRepository extends JpaRepository<Jurnal, String> {
             + "where upper(a.noVoucher) like upper(:s) "
             + "or upper(a.keterangan) like upper(:s) ")
     public Page<Jurnal> filterByKey(@Param("s") String s, Pageable pr);
-    
+
     @Query("from Jurnal a "
-            + "where  ")
-    public List<Jurnal> cariOutstanding(@Param("s") String s, Pageable pr);
+            + "where a.postTime is null and a.postBy is null order by a.tanggal desc")
+    public List<Jurnal> cariOutstanding();
+
+    @Query("from Jurnal a "
+            + "where a.postTime is null and a.postBy is null "
+            + "and (a.tanggal between :tglAwal and :tglAkhir) "
+            + "order by a.tanggal desc")
+    public List<Jurnal> cariOutstandingByTgl(@Param("tglAwal") Date tglAwal, @Param("tglAkhir") Date tglAkhir);
 }
