ce7f76aa4bdf3d3cdaa3123e0dc888e067316b26
faris <faris@thegreatsoft.com>
2017-05-19 20:15:18 +0700

    update directive jqxlist form laporan keuangan

--- src/app/pages/laporan/keuangan/lap-keuangan.controller.js
+++ src/app/pages/laporan/keuangan/lap-keuangan.controller.js
@@ -9,7 +9,7 @@
         var vm = this;
         $scope.tipeSumberDana = 'konsolidasi';
         $scope.tipePeriode = 'bulanan';
-        $scope.reportLabel='Posisi Keuangan';
+        $scope.reportLabel = 'Posisi Keuangan';
         $scope.listBulan = [
             {kode: '01', nama: 'Januari'},
             {kode: '02', nama: 'Februari'},
@@ -27,45 +27,63 @@
         $scope.param = {
             tahun: new Date().getFullYear()
         };
+        var source = [
+            // Utama
+            {html: "<div><div>Posisi Keuangan</div></div>", title: "Posisi Keuangan", group: "Utama"},
+            {html: "<div><div>Aktivitas</div></div>", title: "Aktivitas", group: "Utama"},
+            {html: "<div><div>Arus Kas</div>", title: "Arus Kas", group: "Utama"},
+            // Umum
+            {html: "<div><div>Buku Besar</div></div>", title: "Buku Besar", group: "Umum"},
+            {html: "<div><div>Buku Kas</div></div>", title: "Buku Kas", group: "Umum"},
+            {html: "<div><div>Buku Bank</div></div>", title: "Buku Bank", group: "Umum"},
+            {html: "<div><div>Neraca Percobaan</div></div>", title: "Neraca Percobaan", group: "Umum"},
+            {html: "<div><div>Transaksi</div></div>", title: "Transaksi", group: "Umum"},
+            {html: "<div><div>Aktiva Tetap</div></div>", title: "Aktiva Tetap", group: "Umum"},
+            // Kegiatan
+            {html: "<div><div>Daftar Sumbangan</div></div>", title: "Daftar Sumbangan", group: "Kegiatan"},
+            {html: "<div><div>Status Anggaran Program</div></div>", title: "Status Anggaran Program", group: "Kegiatan"},
+            {html: "<div><div>Status Anggaran Proyek</div></div>", title: "Status Anggaran Proyek", group: "Kegiatan"},
+            {html: "<div><div>Penerimaan dan Pengeluaran Dana</div></div>", title: "Penerimaan dan Pengeluaran Dana", group: "Kegiatan"},
+            {html: "<div><div>Buku Kas Program</div></div>", title: "Buku Kas Program", group: "Kegiatan"},
+            {html: "<div><div>Status Dana Program</div></div>", title: "Status Dana Program", group: "Kegiatan"},
+        ];
+        $scope.settings = {source: source, width: 370, height: 340};
+        $scope.onSelect = function (event) {
+            console.log('onselect event', event);
+            var args = event.args;
+            if (args) {
+                $scope.reportLabel = event.args.item.originalItem.title;
+                console.log('reportLabel', $scope.reportLabel);
+            }
+        };
+
+        $scope.onUnselect = function (event) {
+            console.log('onunselect event', event);
+            var args = event.args;
+            if (args && args.item) {
+                $scope.unselectLog = 'Unselected: ' + args.item.label;
+                console.log('selectLog', $scope.unselectLog);
+            }
+        };
         $(document).ready(function () {
-            var source = [
-                // Utama
-                {html: "<div><div>Posisi Keuangan</div></div>", title: "Posisi Keuangan", group: "Utama"},
-                {html: "<div><div>Aktivitas</div></div>", title: "Aktivitas", group: "Utama"},
-                {html: "<div><div>Arus Kas</div>", title: "Arus Kas", group: "Utama"},
-                // Umum
-                {html: "<div><div>Buku Besar</div></div>", title: "Buku Besar", group: "Umum"},
-                {html: "<div><div>Buku Kas</div></div>", title: "Buku Kas", group: "Umum"},
-                {html: "<div><div>Buku Bank</div></div>", title: "Buku Bank", group: "Umum"},
-                {html: "<div><div>Neraca Percobaan</div></div>", title: "Neraca Percobaan", group: "Umum"},
-                {html: "<div><div>Transaksi</div></div>", title: "Transaksi", group: "Umum"},
-                {html: "<div><div>Aktiva Tetap</div></div>", title: "Aktiva Tetap", group: "Umum"},
-                // Kegiatan
-                {html: "<div><div>Daftar Sumbangan</div></div>", title: "Daftar Sumbangan", group: "Kegiatan"},
-                {html: "<div><div>Status Anggaran Program</div></div>", title: "Status Anggaran Program", group: "Kegiatan"},
-                {html: "<div><div>Status Anggaran Proyek</div></div>", title: "Status Anggaran Proyek", group: "Kegiatan"},
-                {html: "<div><div>Penerimaan dan Pengeluaran Dana</div></div>", title: "Penerimaan dan Pengeluaran Dana", group: "Kegiatan"},
-                {html: "<div><div>Buku Kas Program</div></div>", title: "Buku Kas Program", group: "Kegiatan"},
-                {html: "<div><div>Status Dana Program</div></div>", title: "Status Dana Program", group: "Kegiatan"},
-            ];
 
             // Create a jqxListBox
-            $("#jqxListBox").jqxListBox({source: source, width: 370, height: 340});
-            $('#jqxListBox').on('select', function (event) {
-                var args = event.args;
-                console.log('select', args);
-//                if (args) {
-//                    var index = args.index;
-                    var item = args.item;
-//                    var originalEvent = args.originalEvent;
-                    // get item's label and value.
-                    $scope.reportLabel = item.label;
-//                    alert("label: " + $scope.reportLabel);
-                    console.log('reportLabel', $scope.reportLabel);
-//                    var value = item.value;
-//                    var type = args.type; // keyboard, mouse or null depending on how the item was selected.
-//                }
-            });
+//            $("#jqxListBox").jqxListBox({source: source, width: 370, height: 340});
+//            $('#jqxListBox').on('select', function (event) {
+//                var args = event.args;
+//                console.log('select', args);
+////                if (args) {
+////                    var index = args.index;
+//                    var item = args.item;
+////                    var originalEvent = args.originalEvent;
+//                    // get item's label and value.
+//                    $scope.reportLabel = item.label;
+////                    alert("label: " + $scope.reportLabel);
+//                    console.log('reportLabel', $scope.reportLabel);
+////                    var value = item.value;
+////                    var type = args.type; // keyboard, mouse or null depending on how the item was selected.
+////                }
+//            });
         });
 
     }
